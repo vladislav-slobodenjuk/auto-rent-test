40b24e0c860b887c840ce7ec4f0b25105f255129
vladislav-slobodenjuk <vladjun@gmail.com>
2023-11-01 23:45:08 +0200

    updt router basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/auto-rent-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
