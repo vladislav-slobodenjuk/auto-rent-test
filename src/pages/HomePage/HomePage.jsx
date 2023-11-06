535d36ef96b6f1b742bd4087a061f4acb90d5e94
vladislav-slobodenjuk <vladjun@gmail.com>
2023-11-06 05:45:36 +0200

    font import; readme

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
-import { MainTitle, Section } from './HomePage.styled';
+import { MainTitle, Section, Text } from './HomePage.styled';
 
 const HomePage = () => {
   return (
     <Section>
-      <MainTitle>Welcome to our Car rent service!</MainTitle>
+      <MainTitle>Welcome to our auto rent service!</MainTitle>
+      <Text>Here you can find auto for any price and desires</Text>
       <img
         src="https://www.topgear.com/sites/default/files/2023/09/1%20Ferrari%20Roma%20Spider.jpg?w=1200"
         alt="cool auto"
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -3,8 +3,15 @@ import { StyledSection } from '../CatalogPage/CatalogPage.styled';
 
 export const MainTitle = styled.h1`
   margin-top: 20px;
+  font-size: 30px;
+  text-align: center;
+  color: #d8123a;
+`;
+
+export const Text = styled.p`
   margin-bottom: 30px;
-  font-style: 30px;
+
+  font-size: 20px;
   text-align: center;
   color: #d8123a;
 `;
