fd21ab0b646f3751f76d95c5ce336718fdf911cf
vladislav-slobodenjuk <vladjun@gmail.com>
2023-11-06 00:03:49 +0000

    Deploying to gh-pages from @ vladislav-slobodenjuk/auto-rent-test@4a0173571b777e415654283aff1d5d9186195f46 🚀

---
+++ assets/FavoritesPage-6f114417.js
@@ -0,0 +1,1 @@
+import{u as t,j as e}from"./index-386aaa0b.js";import{d as r,c as a,S as o,A as i}from"./AutoGallery-a27155e5.js";const n=()=>{const s=t(r);return e.jsxs(a,{children:[e.jsx(o,{$hidden:!0,children:"Favorites Gallery"}),e.jsx(i,{cars:s})]})};export{n as default};
---
+++ assets/HomePage-7a70b993.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-386aaa0b.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
